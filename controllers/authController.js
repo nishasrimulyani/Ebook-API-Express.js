07cf8b854dfeae662f8347b865575f5cb035ac68
Amin Nur Azziz <2131710034@student.polinema.ac.id>
2023-12-04 18:54:31 +0700

    repair in app.js, authRouter.js, config/index.js(repair port), already can  register, login

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,44 +17,42 @@ const register = async (input, res) => {
         res.json(error).status(422)
     }
 }
-
 const authentication = async (req, res) => {
     try {
         const email = req.body.email.trim();
         const password = req.body.password.trim();
-        const cekEmail = await Users.findOne({ where: { email: email } });
-        const fetchResult = cekEmail.dataValues
-        const verify = passwordHash.verify(password, fetchResult.password);
 
-        /*  cek apakah password yanng di input sama dengan yang ada didatabase
-            lalu di cocokan menggunakan hash
-        */
-        if (verify != true) {
-            res.json({ msg: 'Password incorect !' }).status(422)
-        } else {
+        // Find the user by email
+        const user = await Users.findOne({ where: { email: email } });
 
-            // isi value token kita mau apa aja
-            const userToken = {
-                id: fetchResult.id,
-                email: fetchResult.email
+        if (!user) {
+            return res.status(422).json({ msg: 'User not found' });
+        }
+
+        // Verify the password
+        const isPasswordValid = passwordHash.verify(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(422).json({ msg: 'Incorrect password' });
+        }
+
+        // Generate a token
+        const jwtKey = process.env.JWT_KEY || 'defaultSecretKey';
+        jwt.sign({ userToken: { id: user.id, email: user.email } }, jwtKey, { expiresIn: '365d' }, (err, token) => {
+            if (err) {
+                console.error('JWT Error:', err);
+                return res.status(500).json({ msg: 'Error generating token', error: err.message });
             }
 
-            /*  set token dengan value usertoken
-                set secret key token kita untuk nanti validasi
-                set expires token nya
-                lalu kasih balikan berupa token jika login sukses
-            */
-            jwt.sign({ userToken }, process.env.JWT_KEY, {
-                expiresIn: '365d' //set exipre token
-            }, (err, token) => {
-                res.json({ token: token }).status(200)
-            });
-        }
+            // Return the token on successful login
+            res.status(200).json({ token: token });
+        });
     } catch (error) {
-        // kondisi jika username atau password salah
-        res.json({ msg: `email or password not match ${error}` }).status(422);
+        console.error('Authentication Error:', error);
+        res.status(500).json({ msg: 'Internal server error', error: error.message });
     }
-}
+};
+
 
 module.exports = {
     register, authentication
